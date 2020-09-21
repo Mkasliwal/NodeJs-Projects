31bc80e83eb35bae30154536f57768feefa47923
mkasliwal <mats.kasliwal@gmail.com>
2020-09-21 13:14:03 +0530

    Initial commit Exercise 1

---
+++ Exercise 1/Starter/app.js
@@ -0,0 +1,11 @@
+var faker = require('faker');
+
+var p1 = faker.commerce.productName();
+var p2 = faker.commerce.price();
+var p3 = faker.commerce.productName();
+var p4 = faker.commerce.price();
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
+console.log(p4);
